d68fcb69a9017b106cf14dfe46bf8740d34bb1a9
UH5555 <zhuhao162@163.com>
2022-03-02 20:44:15 +0800

    0302\操作指示符（一）

--- test.c
+++ test.c
@@ -62,18 +62,99 @@
 //	return 0;
 //}
 
-//不创建临时变量，交换两个int的整型值
-//加减法 - 可能会溢出
-int main()
-{
-	int a = 3;
-	int b = 5;
-	/*a = a + b;
-	b = a - b;
-	a = a - b;*///可能会溢出
-	a = a ^ b;
-	b = a ^ b;
-	a = a ^ b;//异或的方法
-	printf("%d,%d", a, b);
-	return 0;
-}
+////不创建临时变量，交换两个int的整型值
+////加减法 - 可能会溢出
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	/*a = a + b;
+//	b = a - b;
+//	a = a - b;*///可能会溢出
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;//异或的方法
+//	printf("%d,%d", a, b);
+//	return 0;
+//}
+
+////求一个整数存储在内存中1的个数
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);//-1
+//	/*while (num)
+//	{
+//		if (num % 2 == 1)
+//			count++;
+//		num = num / 2;
+//	}*/
+//	//32bit
+//	//num&1 ==1
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num >> i) & 1))
+//		{
+//			count++;
+//		}
+//			
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+////复合赋值符
+//int mian()
+//{
+//	int a = 10;
+//	a = a + 2;
+//	a += 2;//复合赋值符
+//	a = a >> 1;
+//	a >>= 1;
+// }
+
+////单目操作符
+//int main()
+//{
+//	int a = 0;
+//	if (!a)//为假
+//	{
+//		printf("呵呵\n");
+//	}
+//	if (a)//为真
+//	{
+//		printf("哈哈\n")；
+//	}
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;//取地址操作符
+//	*p = 20;//解引用操作符
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	char c = 'r';
+//	char* p = &c;
+//	int arr[10] = { 0 };
+//	//sizeof 计算的变量所占空间的大小，单位是字节
+//	printf("%d\n", sizeof(a));//4
+//	printf("%d\n", sizeof(int));
+//
+//	printf("%d\n", sizeof(c));//1
+//	printf("%d\n", sizeof(char));
+//
+//	printf("%d\n", sizeof(p));//8
+//	printf("%d\n", sizeof(char*));
+//
+//	printf("%d\n", sizeof(arr));//40
+//	printf("%d\n", sizeof(int [10]));
+//
+//	return 0;
+//}
